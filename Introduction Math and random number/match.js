462ee9da2164e6b9402cbddac9964f9e44412cf7
Akib Chy <akibchy1212@gmail.com>
2022-02-01 11:26:48 +0600

    first commit

---
+++ Introduction Math and random number/match.js
@@ -0,0 +1,25 @@
+// const myNunber = -5;
+// const outPut = Math.abs(myNunber);
+// console.log(outPut);
+// let myNunber = 21.1;
+// let outPut = Math.ceil(myNunber);
+// console.log(outPut);
+
+// let outPut = Math.floor(myNunber);
+// console.log(outPut)
+
+// let outPut = Math.round(myNunber);
+// console.log(outPut);
+
+// let MyNumber = 21.54;
+// // let outPut = Math.random();
+// let outPut = Math.random() * 10;
+// let rounded = Math.floor(outPut);
+// console.log(rounded);
+
+for(let i = 0; i<= 2000; i++){
+    let myNunber = 21.46541651;
+    let outPut = Math.random() * 6;
+    let rounded = Math.ceil(outPut);
+    console.log(rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded, rounded, rounded,rounded, rounded);
+}
