b30b40208a31f9d0fe8f172cde9c9304a2efa04b
Akib Chy <akibchy1212@gmail.com>
2022-02-02 21:32:45 +0600

    javascript practis

---
+++ modiul 04 sum of all numbers in array/sum.js
@@ -0,0 +1,32 @@
+// const student1 = [50, 87, 64, 92, 54];
+// const student2 = [50, 87, 64, 92, 55];
+// const student3 = [50, 87, 64, 92, 56];
+
+// function totalNumber(studentNumber){
+//     let stuToNumber = 0;
+//     for(let i =0; i < studentNumber.length; i++){
+//         stuToNumber += studentNumber[i];
+//     }
+//     return stuToNumber;
+// }
+// let TotalNum1 = totalNumber(student1);
+// let TotalNum2 = totalNumber(student3);
+// let TotalNum3 = totalNumber(student3);
+// console.log(TotalNum1, TotalNum2, TotalNum3);
+
+// let myNumber = [50, 50, 50, 50, 50];
+
+// let totalNum = 0;
+// for(let i = 0; i < myNumber.length; i++){
+//    let num1 = myNumber[i];
+//    totalNum += num1;
+// }
+// console.log(totalNum);
+
+const student1 = [51, 51, 51, 51, 51, 51, 51];
+let totalNumber = 0;
+for(let i = 0; i < student1.length; i++){
+    const num1 = student1[i];
+    totalNumber += num1;
+}
+console.log(totalNumber)
