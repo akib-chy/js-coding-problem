779a7b733e8159772b92dcc95ca77008ec1afccd
Akib Chy <akibchy1212@gmail.com>
2022-02-04 00:12:55 +0600

    javascript practis

--- modiul 05 find the largest element of an array/largest-element.js
+++ modiul 05 find the largest element of an array/largest-element.js
@@ -1,11 +1,65 @@
-function largNumber(numbers){
-    let largest = 0;
-    for(let i = 0; i < numbers.length; i++){
+// function largNumber(numbers){
+//     let largest = 0;
+//     for(let i = 0; i < numbers.length; i++){
+//         let element = numbers[i];
+//         largest += element;
+//     }
+//     return largest;
+// };
+// const student = [50, 64, 85, 54, 74];
+// const totalNumber = largNumber(student);
+// console.log(totalNumber)
+
+// function largNumber (number){
+//     let largest = 0;
+//     for(let i=0; i < number.length; i++){
+//         let element = number[i]
+//         largest += element;
+//     }
+//     return largest
+// }
+// let ages = [50, 65, 10, 24, 68, 65]
+// let old = largNumber(ages);
+// console.log(old);
+// function largestElement(numbers){
+//     let largest = 0;
+//     for(let i = 0; i< numbers.length; i++){
+//         const element = numbers[i]
+//             if(element > largest){
+//                 largest = element;
+//             }
+//     }
+//     return largest;
+// }
+// const ages = [210, 54, 6, 85, 20, 47, 94];
+// const oldest = largestElement(ages);
+// console.log(oldest);
+
+// function oldAges(numbers){
+//     let largest = 0;
+//     for( let i = 0; i< numbers.length; i++){
+//         let element = numbers[i];
+//         if(element > largest){
+//             largest = element;
+//         }
+//     }
+//     return largest;
+// }
+// const ages = [21234, 54, 54, 654, 78, 65, 1674,];
+// const oldest = oldAges(ages);
+// console.log(oldest);
+
+
+function oldAges(numbers){
+    let largest = numbers[0];
+    for( let i = 0; i< numbers.length; i++){
         let element = numbers[i];
-        largest += element;
+        if(element > largest){
+            largest = element;
+        }
     }
     return largest;
 }
-const student = [50, 64, 85, 54, 74];
-const totalNumber = largNumber(student);
-console.log(totalNumber)
+const ages = [-21, -20, -2, -14, -1];
+const oldest = oldAges(ages);
+console.log(oldest);
